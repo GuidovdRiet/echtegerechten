0b46195a3fe2f846e46bb6dd17ec91cb9ec89a0c
Jurrian Lammerts <0914922@hr.nl>
2017-03-21 22:13:52 +0100

    added hamburger.js (test)

---
+++ js/hamburger.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".hamburger ").click(function(){
+        $(".overlay").fadeToggle(200);
+    });
+});
+$('.overlay').on('click', function(){
+    $(".overlay").fadeToggle(200);
+    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+    open = false;
+});
+$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
