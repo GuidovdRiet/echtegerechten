6c18255b57dc4280ed1eba1d75ee953817fcdd33
Jurrian Lammerts <0914922@hr.nl>
2017-03-28 14:18:29 +0200

    Improved hamburger

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,22 +1,6 @@
 $(document).ready(function(){
-    $(".hamburger ").click(function(){
-        $(".overlay").fadeToggle(200);
+    $('.hamburger').click(function(){
+        $(this).toggleClass('open');
+        $('.menu-overlay').toggleClass('open');
     });
-});
-$('.overlay').on('click', function(){
-    $(".overlay").fadeToggle(200);
-    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
-    open = false;
-});
-$('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
 });
